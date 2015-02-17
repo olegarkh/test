7f32f984037a694c2ee4a9c95ad4efb6d9d7359d
Oleg <oarch@mail.ru>
2015-02-17 14:37:05 +0300

    создан index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Олег
+ * Date: 17.02.15
+ * Time: 5:14
+ */
+echo "Hello repository!";
---
+++ www/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Олег
+ * Date: 17.02.15
+ * Time: 8:17
+ */ 
